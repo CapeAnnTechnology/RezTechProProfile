c988cf17b7aa840e68422abb061326ad237ba72f
Bradford Knowlton <brad@ucanbonline.net>
2018-11-05 12:14:04 -0500

    switched production urls to profile

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  SERVER_URL: 'https://reztechpro-vip.herokuapp.com',
+  SERVER_URL: 'https://reztechpro-profile.herokuapp.com',
   SSO_URI: 'https://sso.reztechpro.com/',
   API_URI: 'https://reztechpro-api.herokuapp.com/v3.0/',
   BASE_API: 'https://reztechpro-api.herokuapp.com/v3.0/',
-  BASE_URI: 'https://reztechpro-vip.herokuapp.com',
+  BASE_URI: 'https://reztechpro-profile.herokuapp.com',
   AUTH0_CLIENT_DOMAIN: 'reztechpro.auth0.com',
   AUTH0_CLIENT_ID: 'SJLqm1Itq4vSMmc8NhEl59AgV5zVAHTu',
   NAMESPACE: 'https://reztechpro.com'
